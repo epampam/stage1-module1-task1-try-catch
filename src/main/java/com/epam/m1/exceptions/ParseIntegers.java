85ccb30028d04833a83d24b6f31ddab007d32078
epampam <epampam@wp.pl>
2023-02-22 22:02:41 +0100

    finished

--- src/main/java/com/epam/m1/exceptions/ParseIntegers.java
+++ src/main/java/com/epam/m1/exceptions/ParseIntegers.java
@@ -3,11 +3,6 @@ package com.epam.m1.exceptions;
 import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
-
-/**
- * Complete the code, parse integers, calculate the sum of numbers in the WORDS, join strings with
- * space delimiter
- */
 public class ParseIntegers {
 
     private static final List<String> WORDS =
@@ -18,14 +13,51 @@ public class ParseIntegers {
     public static void main(String[] args) {
         Iterator<String> words = WORDS.iterator();
         int sum = 0;
-        String justWords = "";
+        StringBuilder justWords = new StringBuilder();
         while (words.hasNext()) {
             String next = words.next();
-            int number = Integer.parseInt(next);
-            // todo: complete it
+            try {
+                int number = Integer.parseInt(next);
+                sum += number;
+            } catch (NumberFormatException e) {
+                justWords.append(next).append(" ");
+            }
         }
         System.out.println("Sum is " + sum);
-        System.out.println("Just words:" + justWords);
+        System.out.println("Just words: " + justWords.toString());
     }
 }
 
+
+
+///**
+// * Complete the code, parse integers, calculate the sum of numbers in the WORDS, join strings with
+// * space delimiter
+// */
+//public class ParseIntegers {
+//
+//    private static final List<String> WORDS =
+//            Arrays.asList(
+//                    "JDK 17 has released on 14 September 2021 with 10 new features, 2 feature removals and 2 feature deprecations."
+//                            .split(" "));
+//
+//    public static void main(String[] args) {
+//        Iterator<String> words = WORDS.iterator();
+//        int sum = 0;
+//        String justWords = "";
+//        ;
+//        while (words.hasNext()) {
+//            String next = words.next();
+//            try {
+//                int number = Integer.parseInt(next);
+//                sum += number;
+//
+//            } catch (NumberFormatException e) {
+//                justWords = justWords + next + " ";
+//            }
+//        }
+//        System.out.println("Sum is " + sum);
+//        System.out.println("Just words: " + justWords);
+//    }
+//}
+//
